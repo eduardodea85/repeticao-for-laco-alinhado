fdc576a76a208d8ea7d36472670a8cd953af378e
eduardodea85 <eduardodea@outlook.com>
2023-03-01 02:09:15 -0300

    Repeticao-for-laco-alinhado

---
+++ src/repeticaoforalacoalinhado/RepeticaoForaLacoAlinhado.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repeticaoforalacoalinhado;
+
+/**
+ *
+ * @author eduar
+ */
+public class RepeticaoForaLacoAlinhado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        for(int i=1; i<=3; i+=1){
+        }
+        int i = 1;
+        for(int j=0; j<=2; j+=2){
+            System.out.println("i: " + i);
+            System.out.println("j: " + j);
+        
+    }
+       
+    }   
+} 
+
